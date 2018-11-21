6f9d0a5b7430bb9ed5b74ec1a668665bc2df4888
Micke Lisinge <hi@micke.me>
2018-11-21 19:57:40 +0100

    Add

--- githelpers/githelpers.go
+++ githelpers/githelpers.go
@@ -3,72 +3,89 @@ package githelpers
 import (
   "fmt"
   "os"
-  "os/exec"
-  "strings"
-  "log"
 
   "github.com/libgit2/git2go"
 )
 
 func GetCurrentBranch() string{
-  branchCmd := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD")
-  branchArr, err := branchCmd.CombinedOutput()
-  if err != nil {
-    fmt.Printf("Error finding branch: %s %s\n", err, branchArr)
-    os.Exit(1)
-  }
-  return strings.Trim(string(branchArr), "\n")
+  repo := getRepo()
+
+  head, err := repo.Head()
+  checkFail("Failed to get head", err)
+
+  branchName, err := head.Branch().Name()
+  checkFail("Failed to get branch name", err)
+
+  return branchName
 }
 
-func CheckoutBranch(repo *git.Repository, branchName string) error {
+func CheckoutBranch(branchName string) error {
+  var commit *git.Commit
+  repo := getRepo()
   checkoutOpts := &git.CheckoutOpts{
     Strategy: git.CheckoutSafe | git.CheckoutRecreateMissing | git.CheckoutAllowConflicts | git.CheckoutUseTheirs,
   }
 
-  branch, err := repo.LookupBranch(branchName, git.BranchAll)
-  if err != nil {
-    // If branch does not exist we create it!
-    head, err := repo.Head()
-    if err != nil {
-      log.Print("Failed to get head: " + branchName)
-      return err
-    }
+  branch, err := repo.LookupBranch(branchName, git.BranchLocal)
+  if err == nil {
+      fmt.Printf("Switched to branch '%s'\n", branchName)
+  } else {
+    remoteBranch, err := repo.LookupBranch("origin/" + branchName, git.BranchRemote)
+    if err == nil {
+      // Find commit for remote branch
+      commit, err = repo.LookupCommit(remoteBranch.Target())
+      checkFail("Failed to find remote commit", err)
+      fmt.Printf("Found remote branch '%s'\n", branchName)
+    } else {
+      // If remote branch does not exist we create a new local branch
+      head, err := repo.Head()
+      checkFail("Failed to get head", err)
 
-    commit, err := repo.LookupCommit(head.Target())
-    if err != nil {
-      log.Print("Failed to find branch commit: " + branchName)
-      return err
+      commit, err = repo.LookupCommit(head.Target())
+      checkFail("Failed to find local commit", err)
+      fmt.Printf("Creating new branch '%s'\n", branchName)
     }
-    defer commit.Free()
 
     branch, err = repo.CreateBranch(branchName, commit, false)
-    if err != nil {
-      log.Print("Failed to create local branch: " + branchName)
-      return err
+    checkFail("Failed to create local branch", err)
+
+    if remoteBranch != nil {
+      // Set upstream of local branch to remote branch
+      err = branch.SetUpstream("origin/" + branchName)
+      checkFail("Failed to set upstream to origin", err)
     }
   }
-  defer branch.Free()
 
-  commit, err := repo.LookupCommit(branch.Target())
-  if err != nil {
-    log.Print("Failed to find branch commit: " + branchName)
-    return err
-  }
-  defer commit.Free()
+  commit, err = repo.LookupCommit(branch.Target())
+  checkFail("Failed to find commit for branch", err)
 
   tree, err := repo.LookupTree(commit.TreeId())
-  if err != nil {
-    log.Print("Failed to lookup for tree " + branchName)
-    return err
-  }
-  defer tree.Free()
+  checkFail("Failed to lookup for tree", err)
 
   err = repo.CheckoutTree(tree, checkoutOpts)
-  if err != nil {
-    log.Print("Failed to checkout tree " + branchName)
-    return err
-  }
+  checkFail("Failed to checkout tree", err)
 
   repo.SetHead("refs/heads/" + branchName)
   return nil
+}
+
+func getWd() string{
+  dir, err := os.Getwd()
+  checkFail("Error getting current dir", err)
+
+  return dir
+}
+
+func getRepo() *git.Repository{
+  repo, err := git.OpenRepository(getWd())
+  checkFail("Error opening repository", err)
+
+  return repo
+}
+
+func checkFail(message string, err error) {
+  if err != nil {
+    fmt.Printf("%s: %s\n", message, err)
+    os.Exit(1)
+  }
 }
